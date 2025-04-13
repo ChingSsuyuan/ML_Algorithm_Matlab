aa5ba992c75aced9ebea610e476e3303d9b710e3
ChingSsuyuan <crazycyy233@gmail.com>
2025-04-13 13:35:04 -0400

    update all

--- 05 Perceptron Algorithm/test_adult.m
+++ 05 Perceptron Algorithm/test_adult.m
@@ -5,7 +5,12 @@ function test_adult()
     acc_avg = zeros(num_runs,1);
 
     for run = 1:num_runs
-
-
+        m = size(X_train, 1);
+        idx = randperm(m);
+        X_train_shuffled = X_train(idx, :);
+        y_train_shuffled = y_train(idx);
+        [w, b, average_w, average_b] = train_perceptron(X_train_shuffled, y_train_shuffled);
+        y_pred_last = sign(X_test * w + b);
+        acc_last(run) = mean(y_pred_last == y_test);
     end
 end
